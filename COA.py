211cabd3988ca77434caa86a561a82fd2ec8259d
Andrade Rodrigo (C/IDI-LA) <Rodrigo.Andrade2@br.bosch.com>
2019-02-03 17:53:25 -0200

    refactor, use vector instead matrix with a single row

--- COA.py
+++ COA.py
@@ -13,15 +13,12 @@ def COA(FOBJ, lu, nfevalMAX, n_packs=20, n_coy=5):
     Ps = 1/D
 
     pop_total = n_packs*n_coy
-    costs = np.zeros((1, pop_total))
     coyotes = np.tile(VarMin, [pop_total, 1]) + np.random.rand(pop_total, D) * np.tile(VarMax, [pop_total, 1]) - \
               np.tile(VarMin, [pop_total, 1])
-    ages = np.zeros((1, pop_total))
+    ages = np.zeros(pop_total)
     packs = np.random.permutation(pop_total).reshape(n_packs, n_coy)
-    coy_pack = np.tile(n_coy, [n_packs, 1])
 
-    for c in range(pop_total):
-        costs[0, c] = FOBJ(coyotes[c, :])
+    costs = np.array([FOBJ(coyotes[c, :]) for c in range(pop_total)])
 
     nfeval = pop_total
     GlobalMin = min(costs)
@@ -33,9 +30,8 @@ def COA(FOBJ, lu, nfevalMAX, n_packs=20, n_coy=5):
         year += 1
         for p in range(n_packs):
             coyotes_aux = coyotes[packs[p, :], :]
-            costs_aux = costs[0, packs[p, :]]
-            ages_aux = ages[0, packs[p, :]]
-            n_coy_aux = coy_pack[p, 0]
+            costs_aux = costs[packs[p, :]]
+            ages_aux = ages[packs[p, :]]
 
             ind = np.argsort(costs_aux)
             costs_aux = costs_aux[ind]
@@ -46,13 +42,13 @@ def COA(FOBJ, lu, nfevalMAX, n_packs=20, n_coy=5):
             tendency = np.median(coyotes_aux, 0)
             new_coyotes = np.zeros((n_coy, D))
 
-            for c in range(n_coy_aux):
+            for c in range(n_packs):
                 rc1 = c
                 while rc1 == c:
-                    rc1 = np.random.randint(n_coy_aux)
+                    rc1 = np.random.randint(n_packs)
                 rc2 = c
                 while rc2 == c or rc2 == rc1:
-                    rc2 = np.random.randint(n_coy_aux)
+                    rc2 = np.random.randint(n_packs)
 
                 new_coyotes[c, :] = coyotes_aux[c, :] + np.random.rand()*(c_alpha - coyotes_aux[rc1, :]) + \
                                     np.random.rand()*(tendency - coyotes_aux[rc2, :])
@@ -67,25 +63,24 @@ def COA(FOBJ, lu, nfevalMAX, n_packs=20, n_coy=5):
                     costs_aux[c] = new_cost
                     coyotes_aux[c, :] = new_coyotes[c, :]
 
-            parents = np.random.permutation(n_coy_aux)[:2]
+            parents = np.random.permutation(n_packs)[:2]
             prob1 = (1-Ps)/2
             prob2 = prob1
             pdr = np.random.permutation(D)
-            p1 = np.zeros((1, D))
-            p2 = np.zeros((1, D))
-            p1[0, pdr[0]] = 1
-            p2[0, pdr[1]] = 1
-            r = np.random.rand(1, D-2)
-            p1[0, pdr[2:]] = r < prob1
-            p2[0, pdr[2:]] = r > 1-prob2
+            p1 = np.zeros(D)
+            p2 = np.zeros(D)
+            p1[pdr[0]] = 1
+            p2[pdr[1]] = 1
+            r = np.random.rand(D-2)
+            p1[pdr[2:]] = r < prob1
+            p2[pdr[2:]] = r > 1-prob2
 
             n = np.logical_not(np.logical_or(p1, p2))
 
             pup = p1*coyotes_aux[parents[0], :] + \
                   p2*coyotes_aux[parents[1], :] + \
                   n*(VarMin + np.random.rand(1, D) * (VarMax - VarMin))
-
-            pup_cost = FOBJ(pup)
+            pup_cost = FOBJ(pup[0])
             nfeval += 1
 
             worst = np.flatnonzero(costs_aux > pup_cost)
@@ -98,14 +93,13 @@ def COA(FOBJ, lu, nfevalMAX, n_packs=20, n_coy=5):
                 ages_aux[which[0]] = 0
 
             coyotes[packs[p], :] = coyotes_aux
-            costs[0, packs[p]] = costs_aux
-            ages[0, packs[p]] = ages_aux
+            costs[packs[p]] = costs_aux
+            ages[packs[p]] = ages_aux
 
         if n_packs > 1:
             if np.random.rand() < p_leave:
                 rp = np.random.permutation(n_packs)[:2]
-                rc = [np.random.permutation(coy_pack[rp[0], 0])[0], np.random.permutation(coy_pack[rp[1], 0])[0]]
-                rc = np.asarray(rc)
+                rc = np.array([np.random.randint(n_packs), np.random.randint(n_packs)])
                 aux = packs[rp[0], rc[0]]
                 packs[rp[0], rc[0]] = packs[rp[1], rc[1]]
                 packs[rp[1], rc[1]] = aux
@@ -134,7 +128,7 @@ if __name__=="__main__":
     for i in range(100):
         mini, par = COA(f, lu, nfeval, n_packs, n_coy)
         y[0, i] = mini
-        #print(time.time()-t)
+        print(time.time()-t)
         t = time.time()
     print([np.min(y), np.mean(y), np.median(y), np.max(y), np.std(y)])
 
